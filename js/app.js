3cf0d544423fdcaf02e18a829a56ef9d3eb41b67
Juan Salvatore <juansalvatore@live.com.ar>
2018-01-13 19:15:53 -0300

    Add bears animation and background images

--- js/app.js
+++ js/app.js
@@ -49,8 +49,32 @@ document.addEventListener('DOMContentLoaded', event => {
       .to('#blink', 0.04, { opacity: 1, ease: Linear.easeIn }, 1.5)
   }
 
+  bears = () => {
+    var tl = new TimelineMax({
+      paused: false,
+      repeat: -1,
+      yoyo: true,
+      delay: 0,
+    })
+    tl
+      .add('bears')
+      .from('#white-bear', 0.2, { x: 460, ease: Linear.easeInOut }, 3)
+      .to('#white-bear', 0.5, { x: 0, ease: Linear.easeInOut }, 'samsies')
+      .to('#white-bear', 0.5, { x: -10, ease: Linear.easeInOut }, 'samsies')
+      .to('#white-bear', 1, { x: -10, ease: Linear.easeInOut }, 'samsies')
+      .from('#red-bear', 0.2, { x: 460, ease: Linear.easeInOut }, 2.9)
+      .to('#red-bear', 0.5, { x: 0, ease: Linear.easeInOut }, 'samsies')
+      .to('#red-bear', 0.5, { x: -20, ease: Linear.easeInOut }, 'samsies')
+      .to('#red-bear', 1, { x: -20, ease: Linear.easeInOut }, 'samsies')
+      .from('#blue-bear', 0.2, { x: 460, ease: Linear.easeInOut }, 2.95)
+      .to('#blue-bear', 0.5, { x: 0, ease: Linear.easeInOut }, 'samsies')
+      .to('#blue-bear', 0.5, { x: -20, ease: Linear.easeInOut }, 'samsies')
+      .to('#blue-bear', 1, { x: -20, ease: Linear.easeInOut }, 'samsies')
+  }
+
   var master = new TimelineMax()
   master /*.add(fly)*/
     .add(life)
     .add(eyebrow)
+    .add(bears)
 })
